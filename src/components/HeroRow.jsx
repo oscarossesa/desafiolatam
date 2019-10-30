586712267769c67291750cca9c33283bad0a8feb
Oscar Osses <oscar.osses@chilecompra.cl>
2019-10-30 17:52:57 -0300

    Transformo HeroRow de component a functional component

--- src/components/HeroRow.jsx
+++ src/components/HeroRow.jsx
@@ -1,31 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class HeroRow extends Component {
-   render() {
-      const {
-         character,
-         showRings,
-         handleUseRing,
-         handleKill,
-      } = this.props;
+const HeroRow = (props) => {
+   const { character, handleKill, handleUseRing, showRings } = props;
 
-      return (
-         <tr key={character.id} className={`character-row ${character.dead ? 'character-dead' : ''}`}>
-            <td>{character.name}</td>
-            <td>{character.race}</td>
-            <td>{character.age}</td>
-            <td>{character.weapon}</td>
-            <td>
-               <div className='controls'>
-                  <div onClick={handleKill(character.id)}> ☠ Kill</div>
-                  {showRings ?
-                     <div onClick={handleUseRing(character.id)}><span role='img' aria-label='icono'>💍</span> Use Ring</div> :
-                     null}
-               </div>
-            </td>
-         </tr>
-      );
-   }
+   return (
+      <tr key={character.id} className={`character-row ${character.dead ? 'character-dead' : ''}`}>
+         <td>{character.name}</td>
+         <td>{character.race}</td>
+         <td>{character.age}</td>
+         <td>{character.weapon}</td>
+         <td>
+            <div className='controls'>
+               <div onClick={handleKill(character.id)}> ☠ Kill</div>
+               {showRings ?
+                  <div onClick={handleUseRing(character.id)}><span role='img' aria-label='icono'>💍</span> Use Ring</div> :
+                  null}
+            </div>
+         </td>
+      </tr>
+   );
 }
 
 export default HeroRow;
